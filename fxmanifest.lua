0384608bb2e3c87c4e3452ceeebf601463fec96d
Tiw Development <salemmeknez10@gmail.com>
2023-09-03 13:36:04 +0100

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'tiw-input'
+
+version '1.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/styles/*.css',
+    'html/script.js'
+}
+
+lua54 'yes'
